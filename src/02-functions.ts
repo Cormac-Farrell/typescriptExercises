4444e2437ab9bc30fbaf62a58b75cc0397f87cb2
cormacwit <74902128+cormacwit@users.noreply.github.com>
2023-09-22 11:39:08 +0100

    WIP

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -12,25 +12,29 @@ function allOlder(f: Friend[]) : string[] {
 console.log(older(friends[0]))
 console.log(allOlder(friends))
 
-function highestExtension(cs: Colleague[]): Colleague {
-    const result = cs.sort(
-      (c1, c2) => c1.contact.extension - c2.contact.extension
-    );
-    return result[cs.length - 1];
-  }
-  console.log(highestExtension(Colleagues.current));
-
-function addColleagues (cs: Colleague[], name: string, department: string, email: string, extension: number ) : void {
-    const newColleague : Colleague = {
-        name: name,
-        department: department,
-        contact: {
-          email: email,
-          extension: extension,
-        },
-      };
-      cs.push(newColleague);
+function highestExtension(cs: Colleague[]) { // Inferred retun type
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
 }
+
+function addColleagues(
+    ch: Colleague[], 
+    name: string, 
+    department: string, 
+    email: string
+  ): void {
+    const newColleague: Colleague = {
+      name: name,
+      department: department,
+      contact: {
+        email: email,
+        extension: highestExtension(ch).contact.extension + 1
+      }
+    };
+    ch.push(newColleague);
+  }
 
 addColleagues(Colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(Colleagues.current.filter((c) => c.name === "Sheild O Connell"));
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -14,6 +14,6 @@ export interface Colleague {
   }
 
 export interface ColleagueHistory {
-  current: Colleague[],
-  former: Colleague[]
+  current: colleague[],
+  former: colleague[]
 }
